f75264ab493637a71470a9da6eb1201372111841
gndowns <gabriel.downs@shopify.com>
2017-07-24 17:39:22 -0400

    use arrow syntax

--- main.c
+++ main.c
@@ -58,6 +58,18 @@ void animate(int space[], Particles *ps)
   }
 }
 
+void animate_dev(int space[], Particles *ps)
+{
+  int end = 1;
+  while(end) {
+    printf("\n\n");
+    print(space);
+    printf("\n\ncontinue? (1/0): ");
+    scanf("%d", &end);
+    time_step(space, ps);
+  }
+}
+
 void setup(int space[], Particles *ps)
 {
   printf("How many particles would you like to create? (0-%d): ", SIZE);
@@ -75,51 +87,40 @@ void setup(int space[], Particles *ps)
     int v;
     scanf("%d", &v);
 
-    Particle *p = (*ps).list[ (*ps).size ] ;
+    Particle *p = ps->list[ ps->size ];
     p->x = x;
     p->v = v;
-    (*ps).size++;
+    ps->size++;
     space[x] = 1;
   }
   printf("\n");
 }
 
-void animate_dev(int space[], Particles *ps)
-{
-  int end = 1;
-  while(end) {
-    printf("\n\n");
-    print(space);
-    printf("\n\ncontinue? (1/0): ");
-    scanf("%d", &end);
-    time_step(space, ps);
-  }
-}
 
 // simplified prototype, moves ahead 1 second
 void time_step(int space[], Particles *ps)
 {
-  for(int i=0; i < (*ps).size; i++) {
-    move_particle( (*ps).list[i] , space);
+  for(int i=0; i < ps->size; i++) {
+    move_particle( ps->list[i] , space);
   }
 }
 
 void move_particle(Particle *p, int space[])
 {
-  space[ (*p).x ] = 0; 
-  (*p).x += (*p).v;
+  space[ p->x ] = 0;
+  p->x += p->v;
   // keep in bounds
-  (*p).x = (*p).x > (SIZE - 1) ? (SIZE - 1) : (*p).x;
-  (*p).x = (*p).x < 0 ? 0 : (*p).x;
+  p->x = p->x > (SIZE - 1) ? (SIZE - 1) : p->x;
+  p->x = p->x < 0 ? 0 : p->x;
 
-  space[ (*p).x ] = 1;
+  space[ p->x ] = 1;
 }
 
 void add_particle(Particle *p, int space[], Particles *ps)
 {
-  space[ (*p).x ] = 1;
-  (*ps).list[ (*ps).size ] = p;
-  (*ps).size++;
+  space[ p->x ] = 1;
+  ps->list[ ps->size ] = p;
+  ps->size++;
 }
 
 void print(int space[])
