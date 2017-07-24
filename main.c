18f66cc8394792334e7e91fc3c0f9b090985c846
gndowns <gabriel.downs@shopify.com>
2017-07-23 23:51:19 -0400

    simple 1-d animation

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int SIZE = 10;
+int SIZE = 25;
 
-typedef struct Particle{
+typedef struct {
   int x;
   int v;
 } Particle;
 
-typedef struct Particles {
-  Particle list[10];
+typedef struct {
+  Particle *list;
   int size;
 } Particles;
 
-void time_step(int space[], Particles particles);
+void time_step(int space[], Particles *particles);
 void move_particle(Particle *p, int space[]);
 void add_particle(Particle *p, int space[], Particles *ps);
 void print(int space[]);
@@ -20,8 +21,8 @@ void print(int space[]);
 int main() 
 {
   // initialize grids
-  Particles particles;
-  int space[10];
+  Particles particles = {malloc(sizeof(Particle) * SIZE), 0};
+  int space[SIZE];
   for(int i=0; i < SIZE; i++) {
     space[i] = 0;
   }
@@ -31,20 +32,26 @@ int main()
   add_particle(&p1, space, &particles);
 
   // print each square of grid
-  print(space);
-  time_step(space, particles);
-  printf("\n");
-  print(space);
-  printf("\n");
+  int end = 1;
+  /* printf("\n"); */
+  while(end) {
+    /* printf("\b\033[2A\r"); */
+    printf("\n\n");
+    print(space);
+    printf("\n\ncontinue? (1/0): ");
+    /* printf("\n\033[Kcontinue? (1/0): "); */
+    scanf("%d", &end);
+    time_step(space, &particles);
+  }
 
   return 0;
 }
 
 // simplified prototype, moves ahead 1 second
-void time_step(int space[], Particles ps)
+void time_step(int space[], Particles *ps)
 {
-  for(int i=0; i < ps.size; i++) {
-    move_particle( &(ps.list[i]), space);
+  for(int i=0; i < (*ps).size; i++) {
+    move_particle( &( (*ps).list[i] ), space);
   }
 }
 
@@ -52,20 +59,26 @@ void move_particle(Particle *p, int space[])
 {
   space[ (*p).x ] = 0; 
   (*p).x += (*p).v;
+  // keep in bounds
+  (*p).x = (*p).x > (SIZE - 1) ? (SIZE - 1) : (*p).x;
+  (*p).x = (*p).x < 0 ? 0 : (*p).x;
+
   space[ (*p).x ] = 1;
 }
 
 void add_particle(Particle *p, int space[], Particles *ps)
 {
   space[ (*p).x ] = 1;
-  (*ps).list[(*ps).size] = *p;
+  (*ps).list[ (*ps).size ] = *p;
   (*ps).size++;
 }
 
 void print(int space[])
 {
+  printf("[");
   for(int i=0; i < SIZE; i++) {
     int c = space[i] == 0 ? '-' : 'O';
     printf(" %c ", c);
   }
+  printf("]");
 }
