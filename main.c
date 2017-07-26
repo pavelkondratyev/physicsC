c8a5126fae8b6a3b2c04c892a43e1ee1d09cc40d
gndowns <gabriel.downs@shopify.com>
2017-07-25 20:47:19 -0400

    add simple motion

--- main.c
+++ main.c
@@ -5,32 +5,36 @@
 #include<stdlib.h>
 
 const int SIZE = 25;
+const int TIME = 1;
 
 typedef struct {
   int on;
   int v;
   int a;
+  int moved;
 } Cell;
 
-void animate(Cell* []);
-void animate_dev(Cell* []);
-void print(Cell* []);
+void animate(Cell *);
+void animate_dev(Cell *);
+void time_step(Cell *);
+void nullify(Cell *, int);
+void print(Cell *);
 
 int main() 
 {
   // initialize grids
-  Cell **space = malloc( sizeof(Cell*) * SIZE);
+  Cell *space = malloc( sizeof(Cell) * SIZE);
   for(int i=0; i < SIZE; i++) {
-    space[i] = malloc( sizeof(Cell) );
-    space[i]->on = 0;
-    space[i]->v = 0;
-    space[i]->a = 0;
+    space[i].on = 0;
+    space[i].v = 0;
+    space[i].a = 0;
+    space[i].moved = 0;
   }
 
   // add particles
   #ifdef DEFAULT
-    Cell *c = space[0];
-    c->on = 1;
+    space[0].on = 1;
+    space[0].v = 1;
   #endif
   
   #ifdef NO_ANIMATE
@@ -44,7 +48,7 @@ int main()
   return 0;
 }
 
-void animate(Cell *space[])
+void animate(Cell *space)
 {
   int end = 1;
   printf("\n\n");
@@ -53,10 +57,11 @@ void animate(Cell *space[])
     print(space);
     printf("\n\033[Kcontinue? (1/0): ");
     scanf("%d", &end);
+    time_step(space);
   }
 }
 
-void animate_dev(Cell *space[])
+void animate_dev(Cell *space)
 {
   int end = 1;
   while(end) {
@@ -64,14 +69,57 @@ void animate_dev(Cell *space[])
     print(space);
     printf("\n\ncontinue? (1/0): ");
     scanf("%d", &end);
+    time_step(space);
   }
 }
 
-void print(Cell *space[])
+void time_step(Cell *space)
+{
+  for (int i=0; i < SIZE; i++) {
+    if (space[i].on && !space[i].moved) {
+      Cell p = space[i];
+
+      // kinematics
+      int x = i + p.v * TIME + 0.5 * p.a * TIME * TIME;
+      p.v += p.a * TIME;
+
+      // keep in bounds
+      x = x > (SIZE - 1) ? (SIZE - 1) : x;
+      x = x < 0 ? 0 : x;
+
+      // mark as already moved
+      p.moved = 1;
+
+      // update location in grid
+      if (x) {
+        nullify(space, i);
+        space[x] = p;
+      }
+      else {
+        space[i] = p;
+      }
+    }
+  }
+
+  // reset moved (remove this later)
+  for (int i=0; i < SIZE; i++) {
+    space[i].moved = 0;
+  }
+}
+
+void nullify(Cell *space, int i)
+{
+  Cell *p = &(space[i]);
+  p->on = 0;
+  p->v = 0;
+  p->a = 0;
+}
+
+void print(Cell *space)
 {
   printf("[");
   for (int i=0; i < SIZE; i++) {
-    int c = space[i]->on ? 'O' : '-';
+    int c = space[i].on ? 'O' : '-';
     printf(" %c ", c);
   }
   printf("]");
